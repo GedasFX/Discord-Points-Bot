d011edec96ce033c8c4e0d523034ed8e88885e89
GedasFX <contact@gedas.dev>
2025-09-01 19:34:23 +0300

    change directory of db

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("./bot.db");
+const db = new sqlite3.Database("./data/bot.db");
 
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS users (
